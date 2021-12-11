62c47b2f5f08027f59ceed94107249a10f29d1f2
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-11 10:20:14 +0200

    improved saar exchange rated project , final version

--- main.py
+++ main.py
@@ -3,33 +3,25 @@ import json
 
 
 def prod_exchange():
-    currency_names = []
     api_call = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()  # no spaces, http and not https
     get_rates = api_call['rates']
-    for k, v in get_rates.items():
-        if v < 10:
-            currency_names.append(k)
-    return json.dumps(currency_names, indent=4)
+    currency_list = list ((k) for k, v in get_rates.items() if v < 10)
+    return json.dumps(currency_list, indent=4)
 
 
 print (prod_exchange())
 
+
 def dev_exchange():
-    print('dev')
-    currency_names = []
     parsed = json.load(open('all_currency_file_new.json', "r"))
     get_rates = parsed["rates"]
-    for k, v in get_rates.items():
-        if v < 10:
-            currency_names.append(k)
-    return json.dumps(currency_names, indent=4)
-
+    currency_list = list(k for k, v in get_rates.items() if v < 10)
+    return json.dumps(currency_list, indent=4)
 
 
 print(dev_exchange())
 
-
-#i've used the text below one time to create json file with all of the currency in the api
+# i've used the text below one time to create json file with all the currency in the api
 
 '''
 
@@ -41,7 +33,7 @@ def dev_exchange():
         currency_names.append(k)
 
     with open('all_currency_file_new.json', 'w') as outfile:
-        json.dump(api_call, outfile)
+        json.dump(api_call, outfile, indent=4)
 
 dev_exchange()
 
--- test_functions.py
+++ test_functions.py
@@ -17,7 +17,6 @@ def test_prod_exchange():
     assert nice_formatted_currency_list not in list_from_main
 
 
-
 @pytest.mark.dev_exchange
 def test_dev_exchange():
     currency_list = []
@@ -26,6 +25,6 @@ def test_dev_exchange():
     for k, v in get_rates.items():
         if v >= 10:
             currency_list.append(k)
-    nice_formatted_currency_list = json.dumps(currency_list,indent=4)
+    nice_formatted_currency_list = json.dumps(currency_list, indent=4)
     list_from_main = main.dev_exchange()
     assert nice_formatted_currency_list not in list_from_main
