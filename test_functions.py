b953705e5b756b569c73c669b8b7c2a14c7ce3db
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-11 10:33:36 +0200

    improved saar exchange rated project , final version

--- test_functions.py
+++ test_functions.py
@@ -6,12 +6,9 @@ import requests
 
 @pytest.mark.prod_exchange
 def test_prod_exchange():
-    currency_list = []
     response = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
     get_rates = response["rates"]
-    for k, v in get_rates.items():
-        if v >= 10:
-            currency_list.append(k)
+    currency_list = list((k, v) for k, v in get_rates.items() if v >= 10)
     nice_formatted_currency_list = json.dumps(currency_list, indent=4)
     list_from_main = main.prod_exchange()
     assert nice_formatted_currency_list not in list_from_main
@@ -19,12 +16,9 @@ def test_prod_exchange():
 
 @pytest.mark.dev_exchange
 def test_dev_exchange():
-    currency_list = []
     response = json.load(open('all_currency_file_new.json', 'r'))
     get_rates = response["rates"]
-    for k, v in get_rates.items():
-        if v >= 10:
-            currency_list.append(k)
+    currency_list = list((k, v) for k, v in get_rates.items() if v >= 10)
     nice_formatted_currency_list = json.dumps(currency_list, indent=4)
     list_from_main = main.dev_exchange()
     assert nice_formatted_currency_list not in list_from_main
