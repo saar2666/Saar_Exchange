284f8cee69dc6a1332feed2f7570feddd3773c70
saar2666 <saar2666@gmail.comgit config --global user.name saar2666>
2021-12-12 08:57:07 +0200

    improved saar exchange rated project ,with docker

--- main.py
+++ main.py
@@ -28,10 +28,6 @@ print(dev_exchange())
 def dev_exchange():
     currency_names = []
     api_call = requests.get('http://api.exchangeratesapi.io/v1/latest?access_key=36d4d7b23910dc84442f4bc147637fab').json()
-    get_rates = api_call["rates"]
-    for k, v in get_rates.items():
-        currency_names.append(k)
-
     with open('all_currency_file_new.json', 'w') as outfile:
         json.dump(api_call, outfile, indent=4)
 
